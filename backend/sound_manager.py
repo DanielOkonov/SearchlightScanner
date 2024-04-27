9e24bdb7d3726d6d60a01a3bff39bd07b8d188b1
mikkosio <mikkosio3129@gmail.com>
2024-04-26 23:54:36 -0700

    added cooldown on operator alerts

--- backend/sound_manager.py
+++ backend/sound_manager.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 class SoundManager:
     def __init__(self):
@@ -6,12 +7,22 @@ class SoundManager:
             "default": "../sounds/beep.mp3",
             "powerline": "../sounds/Alarm__Missile_Jettison.ogg.mp3"
         }
+        self.last_play_time = 0
+        self.cooldown = 0
 
-    async def play_sound(self, detections):
+    def play_sound(self, detections):
+        # check if enough time has passed since last sound
+        if time.time() - self.last_play_time < self.cooldown:
+            return
+
+        # play sound based on detections
         if detections:
             sound = self.sounds["default"]
+            self.cooldown = 2
             for detection in detections:
                 if detection.label == 5:
                     sound = self.sounds["powerline"]
+                    self.cooldown = 9
                     break
             os.system(f"mpg123 {sound} > /dev/null 2>&1")
+            self.last_play_time = time.time()
